19b46cb1fe16bb465149219dd0c64857b8945c5a
Jean Barbaroux <55790634+jeanBarbaroux@users.noreply.github.com>
2026-03-30 14:13:32 +0200

    ✨ feat(debug#00002): division en folder des cubes
    * Modification pour trier cube par folder
    
    * remove unused import

--- src/shared/props/CubeProps.ts
+++ src/shared/props/CubeProps.ts
@@ -1,9 +1,6 @@
-import type {useCreateStore} from "leva";
-
 export type CubeProps = {
     position?: [number, number, number],
     scale?: [number, number, number],
     texture?: string,
-    debug?: boolean,
-    debugStore?: ReturnType<typeof useCreateStore>
+    name?: string
 }
